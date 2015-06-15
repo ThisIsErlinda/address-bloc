11221131a1a70b9208b17a4f71b92e2c1ae54b38
Erlinda <thisiserlinda@gmail.com>
2015-06-15 12:14:21 -0500

    assignment 24 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
-require_relative "entry.rb"
-require "csv"
+require_relative 'entry.rb'
+require 'csv'
 
 class AddressBook
   attr_accessor :entries
@@ -10,27 +10,55 @@ class AddressBook
 
   def add_entry(name, phone, email)
     index = 0
-
-    @entries.each do |entry|
-      if name < entry.name
-        break
+      @entries.each do |entry|
+        if name < entry.name
+          break
+        end
+        index += 1
       end
-      index += 1
-    end
-
-    @entries.insert(index, Entry.new(name, phone, email))
+      @entries.insert(index, Entry.new(name, phone, email))
+  end
+  
+   def remove_entry(entry)
+    @entries.delete(entry)
   end
 
-  def import_from_csv(file_name)
-    csv_text = File.read(file_name)
+  def binary_search(name)
+    # #1
+     lower = 0
+     upper = entries.length - 1
+
+ # #2
+     while lower <= upper
+ # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+ # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+         return nil
+  end
+
+  def search(name)
+    # Maybe we have more than one implementation of search? 
+    return binary_search(name)
+  end
+
+   def import_from_csv(filename)
+    csv_text = File.read(filename)
     csv = CSV.parse(csv_text, headers: true)
 
     csv.each do |row|
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
- 
-    csv.count
+    return csv.count
   end
-
 end
